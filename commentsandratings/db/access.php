a699fdef1377eea5b61c90a656a5af01812bc848
Chevijan <nikolicm.vuk@gmail.com>
2015-12-10 01:29:23 +0100

    Files under DB folder
    These are v_1 files (default with basic settings) for db folder.
    Will be changed after we find out what will be used in this plugin.

--- commentsandratings/db/access.php
+++ commentsandratings/db/access.php
@@ -3,21 +3,33 @@
  * Capabilities
  *
  * @package    mod_commentsandratings
- * @copyright  2015 Vuk Nikolic
+ * @copyright  2015 Vuk Nikolic <nikolicm.vuk@gmail.com>
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
 defined('MOODLE_INTERNAL') || die();
 
 $capabilities = array(
-    'mod/commentsandratings:addinstance_rating' => array(
+    'mod/commentsandratings:addinstance' => array(
         'riskbitmask' => RISK_XSS | RISK_SPAM,
         'captype' => 'write',
-        'contextlevel' => CONTEXT_COURSE,
+        'contextlevel' => CONTEXT_MODULE,
         'archetypes' => array(
             'student' => CAP_ALLOW,
             'teacher' => CAP_ALLOW,
             'editingteacher' => CAP_ALLOW,
             'manager' => CAP_ALLOW
-    )
+        )
+    ),
+
+    'mod/commentsandratings:view' => array(
+        'captype' => 'read',
+        'contextlevel' => CONTEXT_MODULE,
+        'archetypes' => array(
+            'student' => CAP_ALLOW,
+            'teacher' => CAP_ALLOW,
+            'editingteacher' => CAP_ALLOW,
+            'manager' => CAP_ALLOW
+        )
+    ),
 );
---
+++ commentsandratings/db/upgrade.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * This file is blank for now. When we change version of plugin, this will be changed.
+*/
+
+/**
+ * This file keeps track of upgrades to the commentsandratings module
+ *
+ * @package    mod_commentsandratings
+ * @copyright  2015 Vuk Nikolic <nikolicm.vuk@gmail.com>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
