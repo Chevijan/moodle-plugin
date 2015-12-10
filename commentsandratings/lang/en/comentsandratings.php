49d76b8e82842d622cf09dfff8ea44f97c1ced49
dzulic <julijaciric93@hotmail.com>
2015-12-10 23:57:43 +0100

    ADD LANG - ispravka

--- commentsandratings/lang/en/comentsandratings.php
+++ commentsandratings/lang/en/comentsandratings.php
@@ -1,6 +1,17 @@
 <?php
 
-// Ime plugina
+/**
+ * Defines the version and other meta-info about the plugin
+ *
+ * Setting the $plugin->version to 0 prevents the plugin from being installed.
+ * See https://docs.moodle.org/dev/version.php for more info.
+ *
+ * @package    mod_commentsandratings1
+ * @copyright  2015 Julija Ciric <julijaciric93@gmail.com>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+// Plugin name
 $string['plugin_name']='Comments and ratings';
 
 // Administrator
@@ -18,4 +29,3 @@ $string['rating_users'] ='Rated by {$a} user/s'; //obratiti paznju na promenjive
 $string['leave_a_comment']='Comment';
 $string['leave_a_reply']='Reply';
 
-?>
--- commentsandratings/lang/srb/Komentariiocene.php
+++ commentsandratings/lang/srb/Komentariiocene.php
@@ -1,5 +1,16 @@
 <?php
 
+
+/**
+ * Defines the version and other meta-info about the plugin
+ *
+ * Setting the $plugin->version to 0 prevents the plugin from being installed.
+ * See https://docs.moodle.org/dev/version.php for more info.
+ *
+ * @package    mod_commentsandratings1
+ * @copyright  2015 Julija Ciric <julijaciric93@gmail.com>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
 // Ime plugina
 $string['plugin_name']='Komentari i ocene';
 
@@ -17,5 +28,3 @@ $string['rating_users'] ='Ocenjeno od {$a} korisnika'; //obratiti paznju na prom
 
 $string['leave_a_comment']='Komentar';
 $string['leave_a_reply']='Odgovor';
-
-?>
