fe6847d465c233e1eeb3ef6c143cfe9dde91c277
dzulic <julijaciric93@hotmail.com>
2015-12-10 20:29:59 +0100

    Dodat Lang folder
    Eng i srb verzija. Dodacemo jos promenjivih ako nam je potrebno
    funkcija.

---
+++ commentsandratings/lang/srb/Komentariiocene.php
@@ -0,0 +1,21 @@
+<?php
+
+// Ime plugina
+$string['plugin_name']='Komentari i ocene';
+
+// Administrator
+$string['new_comment_admin']='Novi komentari';
+$string['approve']='Odobri';
+$string['disapprove']='Odbij';
+$string['best_resources']='Najbolje ocenjeni materijali';
+$string['worst_resources']='Najlošije ocenjeni materijali';
+
+// User
+$string['give_a_rating']='Oceni';
+$string['total_rating']='Ukupno';
+$string['rating_users'] ='Ocenjeno od {$a} korisnika'; //obratiti paznju na promenjive
+
+$string['leave_a_comment']='Komentar';
+$string['leave_a_reply']='Odgovor';
+
+?>
