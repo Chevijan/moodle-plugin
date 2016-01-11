66d160770a8df9de1167021c9901c90b2000fd98
dzulic <julijaciric93@hotmail.com>
2016-01-11 13:03:39 +0100

    Prepravka lang

--- commentandrating/lang/en/commentandrating.php
+++ commentandrating/lang/en/commentandrating.php
@@ -40,3 +40,33 @@ $string['commentandrating_help'] = 'Use the commentandrating module for adding n
 $string['name'] = 'Name';
 $string['nocommentsandratings'] = 'No Comments and Ratings';
 $string['sectionname'] = 'Section Name';
+
+
+/**
+ * Defines the version and other meta-info about the plugin
+ *
+ * Setting the $plugin->version to 0 prevents the plugin from being installed.
+ * See https://docs.moodle.org/dev/version.php for more info.
+ *
+ * @package    mod_commentsandratings1
+ * @copyright  2015 Julija Ciric <julijaciric93@gmail.com>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+// Plugin name
+$string['plugin_name']='Comments and ratings';
+
+// Administrator
+$string['newcommentadmin']='New Comments';
+$string['approve']='Approve';
+$string['disapprove']='Disapprove';
+$string['bestresources']='The best rated resources';
+$string['worstresources']='The worst rated resources';
+
+// User
+$string['givearating']='Your rating';
+$string['totalrating']='Total';
+$string['ratingusers'] ='Rated by {$a} user/s'; //obratiti paznju na promenjive
+
+$string['leaveacomment']='Comment';
+$string['leaveareply']='Reply';
