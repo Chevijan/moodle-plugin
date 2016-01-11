7b8ddee06f41672088cc65361ebe3cc00db310f5
dzulic <julijaciric93@hotmail.com>
2016-01-11 11:54:34 +0100

    Promena u lang folderu

---
+++ commentsandratings/lang/en/comentandrating.php
@@ -0,0 +1,45 @@
+<?php
+
+/**
+ * Defines the version and other meta-info about the plugin
+ *
+ * Setting the $plugin->version to 0 prevents the plugin from being installed.
+ * See https://docs.moodle.org/dev/version.php for more info.
+ *
+ * @package    mod_commentsandratings1
+ * @copyright  2015 Julija Ciric <julijaciric93@gmail.com>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+defined('MOODLE_INTERNAL') || die();
+// Plugin name
+$string['plugin_name']='Comments and ratings';
+
+// Administrator
+$string['new_comment_admin']='New Comments';
+$string['approve']='Approve';
+$string['disapprove']='Disapprove';
+$string['best_resources']='The best rated resources';
+$string['worst_resources']='The worst rated resources';
+
+// User
+$string['give_a_rating']='Your rating';
+$string['total_rating']='Total';
+
+
+$string['leave_a_comment']='Comment';
+$string['leave_a_reply']='Reply';
+
+/**
+ * @package    mod_commentandrating
+ * @copyright  2016 Vuk Nikolic <nikolicm.vuk@gmail.com>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+
+$string['modulename'] = 'Comment and Rating';
+$string['modulenameplural'] = 'Comments and Ratings';
+$string['modulename_help'] = 'Use the Comment and Rating module for... ';
+$string['pluginadministration'] = 'Coment and Rating administration';
+$string['pluginname'] = 'Comment and Rating';
+
+
